ad0ddb78d25eadf8dbeafe713c701336768274b6
David <david.schlemmer@uabc.edu.mx>
2025-11-13 12:31:06 -0800

    prueba inicial

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by GF76 on 13/11/2025.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by GF76 on 13/11/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
